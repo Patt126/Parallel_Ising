214861e86b0b63a979bf0880404ebccab2ada83f
Patt126 <145294744+Patt126@users.noreply.github.com>
2024-01-22 16:34:35 +0000

    update mpi final

--- MPI/Lattice/SquareLattice.d
+++
@@ -1,6 +0,0 @@
-/Users/davidepatricelli/Desktop/src/Lattice/SquareLattice.o: \
-  /Users/davidepatricelli/Desktop/src/Lattice/SquareLattice.cpp \
-  /Users/davidepatricelli/Desktop/src/Lattice/SquareLattice.h \
-  /Users/davidepatricelli/Desktop/src/Lattice/AbstractLattice.h
-/Users/davidepatricelli/Desktop/src/Lattice/SquareLattice.h:
-/Users/davidepatricelli/Desktop/src/Lattice/AbstractLattice.h:
